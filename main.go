132dd902d04e1df82605bcbd47b1653621f38e74
Edgar Joel Rivera <126830989+edgarjrivera@users.noreply.github.com>
2024-02-25 13:38:08 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+	server.POST("/events", createEvent)
+
+	server.Run(":8080") // localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		fmt.Printf("Error parsing JSON: %s\n", err.Error()) // Logging the error
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request"})
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
+}
